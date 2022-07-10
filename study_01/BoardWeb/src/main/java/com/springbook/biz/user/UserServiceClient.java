f4b42cdb5f8b1eda2801de64b810ba4daf8c35a7
VanillaDelight <2m5@naver.com>
2022-07-10 22:14:51 +0900

    공부

--- study_01/BoardWeb/src/main/java/com/springbook/biz/user/UserServiceClient.java
+++ study_01/BoardWeb/src/main/java/com/springbook/biz/user/UserServiceClient.java
@@ -15,8 +15,8 @@ public class UserServiceClient {
 		
 		// 3. 로그인 기능 테스트
 		UserVO vo = new UserVO();
-		vo.setId("test");
-		vo.setPassword("test123");
+		vo.setId("user1");
+		vo.setPassword("user1");
 		
 		UserVO user = userService.getUser(vo);
 		if (user != null) {
